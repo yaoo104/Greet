ee78624c817623c611496153834122f2c3c65b35
DannyDog <aleksandryaoo@gmail.com>
2017-09-15 05:11:55 +0300

    Test,Without wrong name method

--- src/test/java/greeting/GreetingTest.java
+++ src/test/java/greeting/GreetingTest.java
@@ -3,7 +3,6 @@ package greeting;
 import org.junit.Assert;
 import org.junit.Test;
 import java.text.SimpleDateFormat;
-
 import java.util.ResourceBundle;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
